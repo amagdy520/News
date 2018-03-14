b74dac62121ff7a322a3a6d28a17a8ebaafa5300
Ahmed Magdy Abd-alzaher <ahmed.magdy.abd.alzaher@gmail.com>
2018-03-14 15:33:57 +0200

    Initial commit

---
+++ app/src/main/java/news/com/news/News.java
@@ -0,0 +1,43 @@
+package news.com.news;
+
+/**
+ * Created by Ahmed Magdy on 22-Feb-18.
+ */
+
+public class News {
+
+    private String title;
+    private String mInformation;
+    private String date;
+    private String mUrl;
+    private String mِAuthorName;
+
+    public News(String mTitle, String mInformation, String mِAuthorName, String mTime, String mUrl) {
+        this.title = mTitle;
+        this.mInformation = mInformation;
+        this.date = mTime;
+        this.mUrl = mUrl;
+        this.mِAuthorName = mِAuthorName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getmInformation() {
+        return mInformation;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public String getAuthorName() {
+        return mِAuthorName;
+    }
+
+}
